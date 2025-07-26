59d8c3f924e1e2ead7ad4049f0f89c2af05093db
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2025-07-25 22:07:00 -0600

    Update submodule reference for Service-Exercise and add transaction control to dummy_data.sql

--- dummy_data.sql
+++ dummy_data.sql
@@ -1,3 +1,5 @@
+BEGIN;
+
 INSERT INTO tema (nombre)
 VALUES ('Introducción'),
 ('Caligrafía'),
@@ -712,3 +714,4 @@ VALUES (1, 2), (2, 2), (3, 2), (4, 2);
 INSERT INTO reactivo_discapacidades (id_reactivo, id_discapacidad)
 VALUES (1, 1), (1, 2), (2, 1), (2, 2), (3, 1), (3, 2), (4, 2), (5, 2), (6, 2), (7, 2), (8, 2), (9, 2), (10, 2);
 
+COMMIT;
